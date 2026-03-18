330d8699c414a204e9539e9918f84e2594fede92
alklin <90446835+alklin@users.noreply.github.com>
2026-03-18 09:42:12 +0700

    Add files via upload

---
+++ Animal 2.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal {
+protected:
+	string name;
+
+public:
+	Animal(string n) {
+		name = n;
+	}
+	void showName() {
+		cout << "Name " << name << endl;
+	}
+	virtual void speak() {
+		cout << "Animal makes a soud " << endl;
+	}
+};
+class Dog : public Animal {
+private:
+	string breed;
+
+public:
+	Dog(string n, string b) : Animal(n) {
+		breed = b;
+	}
+	void showBreed() {
+		cout << "Breed: " << breed << endl;
+	}
+
+		void speak() override {
+			cout << "Dog says: Hi!" << endl;
+		}
+	};
+
+	int main() {
+		Animal a("Unknown");
+		Dog d("Ted", "Border-collie");
+
+		cout << "Base class object " << endl;
+		a.showName(); // базовый
+		a.speak(); // свой
+		d.speak(); // переопределенный
+
+		cout << "n Test Polymorphism test " << endl;
+		Animal* ptr = &d;
+		ptr->speak();
+
+		return 0;
+	}
