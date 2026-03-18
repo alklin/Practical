71b19c157f8a2154868a9f0e45eebf80d3431019
alklin <90446835+alklin@users.noreply.github.com>
2026-03-18 09:34:11 +0700

    Add files via upload

---
+++ Massive N.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    int N;
+    cout << "Enter array size:  ";
+    cin >> N;
+
+    int A[100]; // максимум 100 элементов
+
+    cout << "Enter elements N of array A  ";
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    int maxVal = A[0];
+    int minVal = A[0];
+    int imax = 0, imin = 0;
+
+    for (int i = 1; i < N; i++) {
+        if (A[i] > maxVal) {
+            maxVal = A[i];
+            imax = i;
+        }
+        if (A[i] < minVal) {
+            minVal = A[i];
+            imin = i;
+        }
+    }
+    int start = min(imax, imin) + 1;
+    int end = max(imax, imin);
+
+    int sum = 0;
+
+    for (int i = start; i < end; i++) {
+        if (A[i] < 0) {
+            sum += A[i];
+        }
+    }
+    cout << "Sum of negative elements :"
+        << sum << endl;
+
+    return 0;
+
+}
